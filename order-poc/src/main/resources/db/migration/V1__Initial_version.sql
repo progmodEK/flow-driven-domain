a25be79f133b3ea9a4be66ad5e01e0c3163f006c
elie <elie.khoury.progmod@gmail.com>
2023-12-29 09:50:50 +0100

    Flyway DB init

--- order-poc/src/main/resources/db/migration/V1__Initial_version.sql
+++ order-poc/src/main/resources/db/migration/V1__Initial_version.sql
@@ -1,15 +1,25 @@
-CREATE TABLE IF NOT EXISTS checkout.checkout (
-	id uuid NOT NULL,
-	checkout_data jsonb NOT NULL,
-	CONSTRAINT checkout_pkey PRIMARY KEY (id)
+CREATE SCHEMA IF NOT EXISTS poc AUTHORIZATION "admin";
+
+CREATE TABLE IF NOT EXISTS poc.flow_long (
+	id bigint NOT NULL,
+	flow_data jsonb NOT NULL,
+	CONSTRAINT flow_long_pk PRIMARY KEY (id)
 );
 
-CREATE SEQUENCE IF NOT EXISTS checkout.startup_id_generator;
+CREATE TABLE IF NOT EXISTS poc.flow_uuid (
+	id uuid NOT NULL,
+	flow_data jsonb NOT NULL,
+	CONSTRAINT flow_uuid_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS poc.flow_task (
+	id varchar NOT NULL,
+	score int8 NOT NULL,
+	status varchar NOT NULL,
+	ver int4 NOT NULL,
+	CONSTRAINT flow_task_pk PRIMARY KEY (id)
+);
 
 -- ---------------------------------------------------------------------
 -- INDEXES CREATION MUST NOT BE DONE BY FLYWAY - USE JUST FOR HISTORY --
 -- ---------------------------------------------------------------------
---CREATE INDEX IF NOT EXISTS IDX_ORDER_ID ON checkout.checkout ((checkout_data->'order'->>'order_id'));
---CREATE INDEX IF NOT EXISTS IDX_PROFILE_ID ON checkout.checkout ((checkout_data #>> '{profile,profile_id}'));
---CREATE INDEX IF NOT EXISTS IDX_STATE ON checkout.checkout ((checkout_data->>'state'));
---CREATE INDEX IF NOT EXISTS IDX_CREATED_AT ON checkout.checkout ((checkout_data->>'created_at'));
